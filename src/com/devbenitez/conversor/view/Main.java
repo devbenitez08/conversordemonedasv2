3ead655d73bd62ea9528b9f020b90715b6399317
devbenitez08 <devbenitez08@gmail.com>
2024-06-25 14:30:55 -0600

    Modificando nombre de paquete

--- src/com/devbenitez/conversor/view/Main.java
+++ src/com/devbenitez/conversor/view/Main.java
@@ -1,4 +1,4 @@
-package com.devbenitez.conversor.principal;
+package com.devbenitez.conversor.view;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
